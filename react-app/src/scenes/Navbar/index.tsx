de4ba22621726d759305f4064698df55a03f2c53
Astroneeor <nerroralam@gmail.com>
2024-09-07 19:49:09 -0600

    ReactJS stuff

---
+++ react-app/src/scenes/Navbar/index.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/s";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return <div>index</div>;
+};
---
+++ react-app/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-bg": "#272626",
+        "gray-600": "#343434",
+        "gray-700": "#BDBDBD",
+        "gray-800": "#828282",
+        "primary-100": "#4C9D98",
+        "primary-200": "#3A7D78",
+        "primary-300": "#2A5E5A",
+        "secondary-100": "#FAC61D",
+        "secondary-200": "#E0A21D",
+        "secondary-300": "#C6841D",
+      },
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+      },
+      content: {
+        evolvetest: "url('/src/assets/images/EvolveText.svg')",
+        waves: "url('/src/assets/images/EvolveText.svg')",
+        glow: "url('/src/assets/images/EvolveText.svg')",
+        circles: "url('/src/assets/images/EvolveText.svg')",
+      },
+      screens: {
+        "3xl": "1600px",
+      },
+    },
+    plugins: [],
+  },
+};
