1cc245bd2538a43ca2993ea39a11026f1159ae31
Astroneeor <nerroralam@gmail.com>
2024-09-20 22:29:58 -0600

    IT WORKS

--- react-app/src/pages/Navbar.tsx
+++ react-app/src/pages/Navbar.tsx
@@ -1,29 +1,26 @@
 // src/components/Navbar.tsx
-import React from 'react';
+import React from "react";
 
 const Navbar: React.FC = () => {
   return (
     <nav style={navbarStyles}>
-      <ul style={ulStyles}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/getting-started">Getting Started</a></li>
-      </ul>
+      <div className={`${flexBetween} fixed top-0 z-30 py-6`}></div>
     </nav>
   );
 };
 
 const navbarStyles: React.CSSProperties = {
-  backgroundColor: '#333',
-  padding: '10px',
-  position: 'fixed',
+  backgroundColor: "#333",
+  padding: "10px",
+  position: "fixed",
   top: 0,
-  width: '100%',
+  width: "100%",
 };
 
 const ulStyles: React.CSSProperties = {
-  listStyle: 'none',
-  display: 'flex',
-  justifyContent: 'center',
+  listStyle: "none",
+  display: "flex",
+  justifyContent: "center",
 };
 
 export default Navbar;
